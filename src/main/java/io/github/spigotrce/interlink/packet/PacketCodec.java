29d2000c974d4aae433a772e293c42906939477b
Pranav <128710385+SpigotRCE@users.noreply.github.com>
2025-09-11 13:50:24 +0530

    more and clean basic shit

---
+++ src/main/java/io/github/spigotrce/interlink/packet/PacketCodec.java
@@ -0,0 +1,27 @@
+package io.github.spigotrce.interlink.packet;
+
+import com.google.common.io.*;
+
+import java.util.function.*;
+
+public class PacketCodec<T> {
+  private final Function<ByteArrayDataInput, T> reader;
+  private final BiConsumer<T, ByteArrayDataOutput> writer;
+
+  private PacketCodec(Function<ByteArrayDataInput, T> reader, BiConsumer<T, ByteArrayDataOutput> writer) {
+    this.reader = reader;
+    this.writer = writer;
+  }
+
+  public static <T> PacketCodec<T> of(Function<ByteArrayDataInput, T> reader, BiConsumer<T, ByteArrayDataOutput> writer) {
+    return new PacketCodec<>(reader, writer);
+  }
+
+  public T read(ByteArrayDataInput in) {
+    return reader.apply(in);
+  }
+
+  public void write(T value, ByteArrayDataOutput out) {
+    writer.accept(value, out);
+  }
+}
