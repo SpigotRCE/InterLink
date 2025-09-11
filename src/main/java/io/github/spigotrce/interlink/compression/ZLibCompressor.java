68a3e1d96ec6221e077f8b6b2cfa2bd4f8582285
SpigotRCE <128710385+SpigotRCE@users.noreply.github.com>
2025-09-11 19:23:05 +0530

    feat: ZLibCompressor

---
+++ src/main/java/io/github/spigotrce/interlink/compression/ZLibCompressor.java
@@ -0,0 +1,40 @@
+package io.github.spigotrce.interlink.compression;
+
+import java.io.*;
+import java.util.zip.*;
+
+public class ZLibCompressor {
+  public static byte[] compress(byte[] data) throws IOException {
+    Deflater deflater = new Deflater(Deflater.DEFAULT_COMPRESSION);
+    deflater.setInput(data);
+    deflater.finish();
+
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    byte[] buffer = new byte[512];
+    while (!deflater.finished()) {
+      int count = deflater.deflate(buffer);
+      baos.write(buffer, 0, count);
+    }
+    deflater.end();
+    return baos.toByteArray();
+  }
+
+  public static byte[] decompress(byte[] data) throws IOException {
+    Inflater inflater = new Inflater();
+    inflater.setInput(data);
+
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    byte[] buffer = new byte[512];
+    try {
+      while (!inflater.finished()) {
+        int count = inflater.inflate(buffer);
+        baos.write(buffer, 0, count);
+      }
+    } catch (Exception e) {
+      throw new IOException("Failed to decompress", e);
+    } finally {
+      inflater.end();
+    }
+    return baos.toByteArray();
+  }
+}
--- src/main/java/io/github/spigotrce/interlink/connection/Connection.java
+++ src/main/java/io/github/spigotrce/interlink/connection/Connection.java
@@ -1,26 +1,25 @@
 package io.github.spigotrce.interlink.connection;
 
-import com.google.common.io.*;
 import io.github.spigotrce.interlink.buf.*;
+import io.github.spigotrce.interlink.compression.ZLibCompressor;
 import io.github.spigotrce.interlink.packet.*;
 
 import java.io.*;
 import java.net.Socket;
 
 public class Connection {
-  private static final int GCM_TAG_LENGTH = 128; // bits
-  private static final int GCM_NONCE_LENGTH = 12; // bytes
-
   private final Socket socket;
   private final PacketRegistry registry;
   private final DataOutputStream output;
   private final DataInputStream input;
+  private final int compressionThreshold;
 
-  public Connection(Socket socket, PacketRegistry registry) throws IOException {
+  public Connection(Socket socket, PacketRegistry registry, int compressionThreshold) throws IOException {
     this.socket = socket;
     this.registry = registry;
     this.output = new DataOutputStream(socket.getOutputStream());
     this.input = new DataInputStream(socket.getInputStream());
+    this.compressionThreshold = compressionThreshold;
   }
 
   public void send(Packet<?> packet) throws IOException {
@@ -29,18 +28,36 @@ public class Connection {
     if (id == -1) {
       throw new IOException("Unregistered packet: " + packet.getClass());
     }
+
     out.writeInt(id);
     registry.encode(packet, out);
+
     byte[] data = out.toByteArray();
+
+    boolean compressed = data.length >= compressionThreshold;
+    if (compressed) {
+      data = ZLibCompressor.compress(data);
+    }
+
+    // [compressed flag][length][data]
+    output.writeBoolean(compressed);
     output.writeInt(data.length);
     output.write(data);
     output.flush();
   }
 
   public Packet<?> read() throws IOException {
+    // [compressed flag][length][data]
+    boolean compressed = input.readBoolean();
     int length = input.readInt();
+
     byte[] data = new byte[length];
     input.readFully(data);
+
+    if (compressed) {
+      data = ZLibCompressor.decompress(data);
+    }
+
     InputBuffer in = InputBuffer.create(data);
     int id = in.readInt();
     return registry.decode(id, in);
