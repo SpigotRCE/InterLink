61166512efda06bd53eb08aacd0bc3d0aa439ba0
Pranav <128710385+SpigotRCE@users.noreply.github.com>
2025-09-11 13:54:47 +0530

    made packet registry non-static

--- src/main/java/io/github/spigotrce/interlink/packet/PacketRegistry.java
+++ src/main/java/io/github/spigotrce/interlink/packet/PacketRegistry.java
@@ -5,23 +5,23 @@ import com.google.common.io.*;
 import java.util.*;
 
 public class PacketRegistry {
-  public static final List<PacketEntry<? extends Packet<?>>> packets = new ArrayList<>();
+  public final List<PacketEntry<? extends Packet<?>>> packets = new ArrayList<>();
 
-  public static <T extends Packet<?>> void registerPacket(Class<T> packetClass, PacketCodec<T> codec) {
+  public <T extends Packet<?>> void registerPacket(Class<T> packetClass, PacketCodec<T> codec) {
     packets.add(new PacketEntry<>(packetClass, codec));
   }
 
-  public static void encode(Packet<?> packet, ByteArrayDataOutput out) {
+  public void encode(Packet<?> packet, ByteArrayDataOutput out) {
     @SuppressWarnings("unchecked") PacketEntry<Packet<?>> entry = (PacketEntry<Packet<?>>) packets.get(getId(packet));
     entry.codec().write(packet, out);
   }
 
-  public static Packet<?> decode(int id, ByteArrayDataInput in) {
+  public Packet<?> decode(int id, ByteArrayDataInput in) {
     @SuppressWarnings("unchecked") PacketEntry<Packet<?>> entry = (PacketEntry<Packet<?>>) packets.get(id);
     return entry.codec().read(in);
   }
 
-  public static int getId(Packet<?> packet) {
+  public int getId(Packet<?> packet) {
     for (PacketEntry<? extends Packet<?>> entry : packets) {
       if (entry.clazz().equals(packet.getClass())) {
         return packets.indexOf(entry);
